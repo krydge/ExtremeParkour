a6229447a43bf821d3c5983723c404dc8fbf2545
Kaydon stubbs <k.rydge.stubbs@gmail.com>
2020-03-03 12:24:14 -0700

    Add project files.

---
+++ ExtremeParkour/ExtremeParkour/Views/WorkoutsPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace ExtremeParkour.Views
+{
+    public partial class WorkoutsPage : ContentPage
+    {
+        public WorkoutsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
